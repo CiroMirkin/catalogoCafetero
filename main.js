6d19bfb8e835a67b7c3cac7a7dd570aae9335a03
c.mirkin <ciromirkin@gmail.com>
2024-06-15 11:52:40 -0300

    add: html attributes to toaster elements

--- main.js
+++ main.js
@@ -14,13 +14,13 @@ const showCatalog = (catalog) => {
         <li class="toaster">
             <main>
                 <div class="toaster__logo">
-                    <img src="${toaster.logoUrl}" class="toaster__logo-logo">
+                    <img src="${toaster.logoUrl}" class="toaster__logo-logo" onerror="this.style.width=0;" alt="Logo de ${toaster.name}">
                 </div>
                 <h3>${toaster.name}</h3>
             </main>
             <footer class="info">
-                <a href="${toaster.website}" target="_blank"><i class="icon-globe"></i></a>
-                <a href="${toaster.instagram}" target="_blank"><i class="icon-insta"></i></a>
+                <a href="${toaster.website}" target="_blank" title="Sitio web de ${toaster.name}"><i class="icon-globe"></i></a>
+                <a href="${toaster.instagram}" target="_blank" title="Instagram de ${toaster.name}"><i class="icon-insta"></i></a>
             </footer>
         </li>
     `).join("")
